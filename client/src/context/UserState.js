3fee8a93f97a0b89b35f87e189b9cfcfccf71588
Vaibhav <vaibhavverma21122001@gmai.com>
2023-03-22 23:49:24 +0530

    my first commit

---
+++ client/src/context/UserState.js
@@ -0,0 +1,18 @@
+import UserContext from "./UserContext";
+import { useState } from "react";
+
+const UserState = (props) => {
+    const [user, setUser] = useState(null); //if user then set user else null, parse because in localStorage it is in string format
+    
+    // loading bar
+    const [progress, setProgress] = useState(0);
+    // dark-light mode
+    const [mode, setMode] = useState("light");
+
+    return (
+        <UserContext.Provider value={{ user, setUser,progress ,setProgress, mode ,setMode}}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
+export default UserState;
